fc70ce330ebbe75b1919a1e0999d360c1804c7f7
alfred <alfred.7790@gmail.com>
2019-03-17 01:14:24 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // inicializar varibales
 var app = express();
+
+// body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// import rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 // conexion bd
 mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb', (err, res) => {
@@ -13,12 +25,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb', (err, res) =
 });
 
 // rutas
-app.get('/', (req, res, next) => {
-	res.status(200).json({
-		ok: true,
-		mensaje: 'Petición realizada correctamente'
-	})
-});
+app.use('/usuarios', usuarioRoutes );
+app.use('/login', loginRoutes );
+app.use('/', appRoutes );
+
 
 // escuchar peticiones
 app.listen(3000, () => {
